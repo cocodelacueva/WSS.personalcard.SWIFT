6d81e839b043e1e8a47dac88c1f0411addf638f9
coco <cocodelacueva@gmail.com>
2026-05-15 08:34:56 -0300

    Initial Commit

---
+++ personalcard/personalcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalcardApp.swift
+//  personalcard
+//
+//  Created by coco on 15/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct personalcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
